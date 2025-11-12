9d4290d7fef0288c0fa3731f2abfd1e428491e2f
tivvvv <18612553935@163.com>
2025-11-12 12:18:15 +0800

    feat: RESP数组.

--- src/main/java/com/tiv/jedis/server/protocol/RESPArrays.java
+++ src/main/java/com/tiv/jedis/server/protocol/RESPArrays.java
@@ -1,12 +1,29 @@
 package com.tiv.jedis.server.protocol;
 
 import io.netty.buffer.ByteBuf;
+import lombok.Getter;
 
+/**
+ * RESP数组
+ */
+@Getter
 public class RESPArrays extends RESPProtocol {
+
+    private final RESPProtocol[] content;
+
+    public RESPArrays(RESPProtocol[] content) {
+        this.content = content;
+    }
 
     @Override
     public void encode(RESPProtocol respProtocol, ByteBuf byteBuf) {
-
+        // "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
+        byteBuf.writeByte('*');
+        byteBuf.writeBytes(String.valueOf(content.length).getBytes());
+        for (RESPProtocol resp : content) {
+            resp.encode(resp, byteBuf);
+        }
+        byteBuf.writeBytes(CRLF);
     }
 
 }
