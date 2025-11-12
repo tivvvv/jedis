e74361ce746526ca5ab271b41546b1b1df22712e
tivvvv <18612553935@163.com>
2025-11-12 09:12:04 +0800

    feat: RESP批量字符串.

--- src/main/java/com/tiv/jedis/server/protocol/RESPBulkStrings.java
+++ src/main/java/com/tiv/jedis/server/protocol/RESPBulkStrings.java
@@ -1,12 +1,40 @@
 package com.tiv.jedis.server.protocol;
 
 import io.netty.buffer.ByteBuf;
+import lombok.Data;
 
+/**
+ * RESP批量字符串
+ */
+@Data
 public class RESPBulkStrings extends RESPProtocol {
+
+    private static final byte[] NULL_BYTES = "-1\r\n".getBytes();
+
+    private static final byte[] EMPTY_BYTES = "0\r\n\r\n".getBytes();
+
+    private final byte[] content;
+
+    public RESPBulkStrings(byte[] content) {
+        this.content = content;
+    }
 
     @Override
     public void encode(RESPProtocol respProtocol, ByteBuf byteBuf) {
-
+        // "$6\r\nfoobar\r\n"
+        byteBuf.writeByte('$');
+        if (content == null) {
+            byteBuf.writeBytes(NULL_BYTES);
+        } else {
+            if (content.length == 0) {
+                byteBuf.writeBytes(EMPTY_BYTES);
+            } else {
+                byteBuf.writeBytes(String.valueOf(content.length).getBytes());
+                byteBuf.writeBytes(CRLF);
+                byteBuf.writeBytes(content);
+                byteBuf.writeBytes(CRLF);
+            }
+        }
     }
 
 }
